624e35a217b424d35e0e84003f09384511ef728f
bl00m <tguchi3@gmail.com>
2014-02-26 14:43:36 -0800

    changed from pins to line along route

--- app/scripts/latlon.js
+++ app/scripts/latlon.js
@@ -1,11 +1,11 @@
 var geocoder;
 var map;
-var directionsService;
+var directionsService = new google.maps.DirectionsService();
 var markerArray = []
 
 function initialize() {
   geocoder = new google.maps.Geocoder();
-  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
 
   var latlng = new google.maps.LatLng(47.623, -122.336);
 
@@ -19,16 +19,16 @@ function initialize() {
   }
 
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-  stepDisplay = new google.maps.InfoWindow();
+  // stepDisplay = new google.maps.InfoWindow();
+  directionsDisplay.setMap(map);
 }
 
 function calcRoute() {
-  for (var i = 0; i < markerArray.length; i++) {
-    markerArray[i].setMap(null);
-  }
+  // for (var i = 0; i < markerArray.length; i++) {
+  //   markerArray[i].setMap(null);
+  // }
 
-  markerArray = [];
+  // markerArray = [];
 
   var start = document.getElementById('start').value;
   var end = document.getElementById('end').value;
@@ -40,25 +40,25 @@ function calcRoute() {
 
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-      var warnings = document.getElementById('warnings_panel');
-      warnings.innerHTML = '<b>' + response.routes[0].warnings + '</b>';
+      // var warnings = document.getElementById('warnings_panel');
+      // warnings.innerHTML = '<b>' + response.routes[0].warnings + '</b>';
       directionsDisplay.setDirections(response);
-      showSteps(response);
+      // showSteps(response);
     }
   });
 }
 
-function showSteps(directionResult) {
-  var myRoute = directionResult.routes[0].legs[0];
-  for (var i = 0; i < myRoute.steps.length; i++) {
-    var marker = new google.maps.Marker({
-      position: myRoute.steps[i].start_location,
-      map: map
-    });
-    attachInstructionText(marker, myRoute.steps[i].instructions);
-    markerArray[i] = marker;
-  }
-}
+// function showSteps(directionResult) {
+//   var myRoute = directionResult.routes[0].legs[0];
+//   for (var i = 0; i < myRoute.steps.length; i++) {
+//     var marker = new google.maps.Marker({
+//       position: myRoute.steps[i].start_location,
+//       map: map
+//     });
+//     attachInstructionText(marker, myRoute.steps[i].instructions);
+//     markerArray[i] = marker;
+//   }
+// }
 
 function attachInstructionText(marker, text) {
   google.maps.event.addListener(marker, 'click', function() {
@@ -67,19 +67,19 @@ function attachInstructionText(marker, text) {
   });
 }
 
-function codeAddress(id) {
-  var address = document.getElementById(id).value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+// function codeAddress(id) {
+//   var address = document.getElementById(id).value;
+//   geocoder.geocode( { 'address': address}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       map.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//           map: map,
+//           position: results[0].geometry.location
+//       });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
 
 google.maps.event.addDomListener(window, 'load', initialize);
