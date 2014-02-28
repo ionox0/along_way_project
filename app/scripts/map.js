aea502bf0d731bd9122278c21928b2343937f009
Ewelina <ewelina.kieley@gmail.com>
2014-02-28 12:34:35 -0800

    adding google places

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -5,12 +5,41 @@ window.google_initialize = function () {
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
+    var service;
+    infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+
+  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'best view'
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    alert(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: result.geometry.location
+    });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 };
 
 $(document).ready(function () {
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC2CVRCqI-iLtJ5_MdAlXxeqe8qU193WuI&sensor=false&callback=google_initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC2CVRCqI-iLtJ5_MdAlXxeqe8qU193WuI&libraries=places&sensor=false&callback=google_initialize';
     document.body.appendChild(script);
 });
