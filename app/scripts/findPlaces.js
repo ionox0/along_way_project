d4403b7fbde641c01ab10b6adc80faf92c269dad
ionox0 <ionox0@gmail.com>
2014-03-04 22:14:51 -0800

    implemented clearRoute(), clearMarkers(), clearBoxes()

--- app/scripts/findPlaces.js
+++ app/scripts/findPlaces.js
@@ -1,38 +1,39 @@
 var placesServices;
 var minRating = 1;
+var markers = [];
 
-function findPlaces(boxes, searchIndex, place) {
-   placesServices = new google.maps.places.PlacesService(map);
-   var request = {
-       bounds: boxes[searchIndex],
-// List of supported types
-// https://developers.google.com/places/documentation/supported_types
-       keyword: place
-     }
-   placesServices.radarSearch(request, function (results, status) {
-       if (status != google.maps.places.PlacesServiceStatus.OK) {
-           alert("Request["+searchIndex+"] failed: "+status);
-           return;
-       }
+function findPlaces(boxes, searchIndex) {
 
-       //document.getElementById('side_bar').innerHTML += "bounds["+searchIndex+"] returns "+results.length+" results<br>"
-       for (var i = 0, result; result = results[i]; i++) {
+  var place = document.getElementById('place').value;
+  placesServices = new google.maps.places.PlacesService(map);
+  var request = {
+    bounds: boxes[searchIndex],
+    keyword: 'gas_station'
+  }
+  placesServices.radarSearch(request, function (results, status) {
 
-           //if (placesServices..getDetails(.rating > minRating){
-               var marker = createMarker(result);
-           //}
-       }
-       searchIndex++;
-       if (searchIndex < boxes.length)
-           findPlaces(boxes,searchIndex, place);
-   });
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
+      alert("Request["+searchIndex+"] failed: "+status);
+      return;
+    }
+    for (var i = 0, result; result = results[i]; i++) {
+      var marker = createMarker(result);
+      markers.push(marker);
+    }
+    searchIndex++;
+    if (searchIndex < boxes.length)
+      findPlaces(boxes,searchIndex);
+
+  });
 }
 
 // function clearPlaces() {
-//   if (markers != null) {
+//   if (markers.length != 0) {
+//     console.log(markers.length);
 //     for (var i = 0; i < markers.length; i++) {
+//       console.log(markers[i]);
 //       markers[i].setMap(null);
 //     }
 //   }
-//   markers = null;
+//   markers = [];
 // }
--- app/scripts/route.js
+++ app/scripts/route.js
@@ -1,30 +1,34 @@
+//easier way to set jshint globals?
+var directionsDisplay = new google.maps.DirectionsRenderer();
 var routeBoxer = new RouteBoxer();
+var boxes;
 
 function calcRoute() {
- var directionsService = new google.maps.DirectionsService();
- var directionsDisplay = new google.maps.DirectionsRenderer();
- directionsDisplay.setMap(map);
- clearBoxes();
- clearMarkers();
- var distance = parseFloat(20) * 1.609344;
- var start = document.getElementById('start').value;
- var end = document.getElementById('end').value;
- var place = document.getElementById('place').value;
- var request = {
-   origin:start,
-   destination:end,
-   travelMode: google.maps.TravelMode.DRIVING
- };
- directionsService.route(request, function(result, status) {
-   if (status == google.maps.DirectionsStatus.OK) {
-     directionsDisplay.setDirections(result);
-     var path = result.routes[0].overview_path;
-     var boxes = routeBoxer.box(path, distance);
-     drawBoxes(boxes);
-     findPlaces(boxes,0,place);
-   }
-   else {
-     alert("Directions query failed: " + status);
-   }
- });
+  var directionsService = new google.maps.DirectionsService();
+  directionsDisplay.setMap(map);
+  clearBoxes();
+  clearMarkers();
+  var distance = parseFloat(20) * 1.609344;
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+      var path = result.routes[0].overview_path;
+      boxes = routeBoxer.box(path, distance);
+      drawBoxes(boxes);
+    }
+    else {
+      alert("Directions query failed: " + status);
+    }
+  });
+}
+
+function clearRoute(){
+  directionsDisplay.setMap(null);
 }
