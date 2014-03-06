fa69ca35c217a735926196b500a3f7d44f1e1de8
Ewelina <ewelina.kieley@gmail.com>
2014-03-05 20:58:49 -0800

    search radius working
    Need to make it more user friendly though

--- app/scripts/route.js
+++ app/scripts/route.js
@@ -3,37 +3,39 @@ var directionsDisplay = new google.maps.DirectionsRenderer();
 var routeBoxer = new RouteBoxer();
 var boxes;
 
+
 function calcRoute() {
-  var directionsService = new google.maps.DirectionsService();
-  directionsDisplay.setMap(map);
-  clearBoxes();
-  clearMarkers();
-  var distance = parseFloat(20) * 1.609344;
+    var km = parseFloat(document.getElementById("radius").value) || 1;
+    var miles = km * 1.609344;
+    var directionsService = new google.maps.DirectionsService();
+    directionsDisplay.setMap(map);
+    clearBoxes();
+    clearMarkers();
+    var distance = miles;
 
-  var start = document.getElementById('start').value.trim();
-  var end = document.getElementById('end').value.trim();
-  var place = document.getElementById('place').value.trim();
+    var start = document.getElementById('start').value.trim();
+    var end = document.getElementById('end').value.trim();
+    var place = document.getElementById('place').value.trim();
 
-  var request = {
-    origin:start,
-    destination:end,
-    travelMode: google.maps.TravelMode.DRIVING
-  };
-  directionsService.route(request, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(result);
-      var path = result.routes[0].overview_path;
-      boxes = routeBoxer.box(path, distance);
-      drawBoxes(boxes);
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+            var path = result.routes[0].overview_path;
+            boxes = routeBoxer.box(path, distance);
+            drawBoxes(boxes);
 
-      findPlaces(boxes, 0, place);
-    }
-    else {
-      alert("Directions query failed: " + status);
-    }
-  });
+            findPlaces(boxes, 0, place);
+        } else {
+            alert("Directions query failed: " + status);
+        }
+    });
 }
 
-function clearRoute(){
-  directionsDisplay.setMap(null);
+function clearRoute() {
+    directionsDisplay.setMap(null);
 }
