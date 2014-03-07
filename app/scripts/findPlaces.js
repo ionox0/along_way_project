7c5bb168af19dc69d36dd6fef3e1995bc6ed3384
ionox0 <ionox0@gmail.com>
2014-03-06 19:10:26 -0800

    yelp

--- app/scripts/findPlaces.js
+++ app/scripts/findPlaces.js
@@ -1,36 +1,57 @@
 var placesServices;
-var place = document.getElementById('place').value;
+
 
 function findPlaces(boxes, searchIndex) {
+    placesServices = new google.maps.places.PlacesService(map);
     var nowOpen = document.getElementById('checkbox1').value ;
     var maxPrice = document.getElementById('max-price').value || 4;
     var place = document.getElementById('place').value;
-    placesServices = new google.maps.places.PlacesService(map);
     var request = {
         bounds: boxes[searchIndex],
         keyword: place,
         maxPriceLevel: maxPrice,
         openNow: nowOpen
-    }
+    };
     placesServices.radarSearch(request, function (results, status) {
-
+      console.log(results);
         if (status != google.maps.places.PlacesServiceStatus.OK) {
-            alert("Request[" + searchIndex + "] failed: " + status);
-            return;
+            console.log("Request[" + searchIndex + "] failed: " + status);
+        }else{
+          for (var i = 0, result; result = results[i]; i++) {
+            console.log("boxes " + searchIndex + ", result: " + i + " " + result);
+            getRating(result);
+          }
         }
-        for (var i = 0, result; result = results[i]; i++) {
-
-            //if (result.rating != null){
-            var marker = createMarker(result);
-
-            markers.push(marker);
-            //}
-        }
-        // console.log(placesServices.getDetails());
-
         searchIndex++;
         if (searchIndex < boxes.length)
             findPlaces(boxes, searchIndex);
-
     });
+}
+
+function getRating(inputPlace){
+  console.log(inputPlace);
+  var place = inputPlace;
+  var request = {
+    reference: place.reference
+  };
+
+  placesServices.getDetails(request, function(place, status) {
+
+    var address = place.formatted_address;
+    var name = place.name;
+    var yelp_key = "vhGs7ntrDsYquBIwzmouag";
+    var yelpURL = "http://api.yelp.com/business_review_search?callback=?";
+    var yelp_params = {
+        "location": address,
+        "id": name,
+        "ywsid": yelp_key
+    };
+    //console.log(yelp_params);
+    $.getJSON(yelpURL, yelp_params, function(results) {
+      //if(results.businesses[0].rating > 1){
+      if (results.businesses[0].avg_rating > 4){        //set to a slider value
+        createMarker(place);
+      }
+    });
+  });
 }
