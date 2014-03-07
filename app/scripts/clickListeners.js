2b1e8c3f6a0101ebaa0ab7b7d6095d30cbcf12d1
Ewelina <ewelina.kieley@gmail.com>
2014-03-07 00:26:44 -0800

    fixed filter buttons

--- app/scripts/clickListeners.js
+++ app/scripts/clickListeners.js
@@ -1,9 +1,9 @@
-$('#search-route-button').click(function(e){
+$('.search-route-button').click(function(e){
 
     calcRoute();
     e.preventDefault();
 });
-$('#search-places-button').click(function(e){
+$('.search-places-button').click(function(e){
     clearMarkers();
     findPlaces(boxes,0);
     e.preventDefault();
--- app/scripts/findPlaces.js
+++ app/scripts/findPlaces.js
@@ -1,18 +1,30 @@
 var placesServices;
 
-$("nav #filter").click(function() {
+$("nav #filter").click(function () {
     $(".filterForm").addClass("shown");
 });
-             $("#hide").click(function() {
+$("#hide").click(function () {
     $(".filterForm").removeClass("shown");
 });
-
+var maxPrice = 4;
+    $("#dollar1").click(function () {
+        maxPrice = 1;
+    }),
+    $("#dollar2").click(function () {
+        maxPrice = 2;
+    }),
+    $("#dollar3").click(function () {
+        maxPrice = 3;
+    }),
+    $("#dollar4").click(function () {
+        maxPrice = 4;
+    });
 
 function findPlaces(boxes, searchIndex) {
     placesServices = new google.maps.places.PlacesService(map);
-    var nowOpen = document.getElementById('checkbox1').value ;
-    var maxPrice = document.getElementById('max-price').value || 4;
+    var nowOpen = document.getElementById('checkbox1').checked;
     var place = document.getElementById('place').value;
+    
     var request = {
         bounds: boxes[searchIndex],
         keyword: place,
@@ -20,15 +32,15 @@ function findPlaces(boxes, searchIndex) {
         openNow: nowOpen
     };
     placesServices.radarSearch(request, function (results, status) {
-      //console.log(results);
+        //console.log(results);
         if (status != google.maps.places.PlacesServiceStatus.OK) {
             console.log("Request[" + searchIndex + "] failed: " + status);
-        }else{
-          for (var i = 0, result; result = results[i]; i++) {
-            console.log("boxes " + searchIndex + ", result: " + i + " " + result);
-            //getRating(result);
-            createMarker(result);
-          }
+        } else {
+            for (var i = 0, result; result = results[i]; i++) {
+                console.log("boxes " + searchIndex + ", result: " + i + " " + result);
+                //getRating(result);
+                createMarker(result);
+            }
         }
         searchIndex++;
         if (searchIndex < boxes.length)
