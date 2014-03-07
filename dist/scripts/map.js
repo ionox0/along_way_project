650f64891cf07f76018e78c21060b6f97824333c
8legged <jason.niemi@live.com>
2014-03-06 19:38:36 -0800

    grunt build

--- dist/scripts/map.js
+++ dist/scripts/map.js
@@ -37,9 +37,13 @@ function initialize() {
 
     var end_complete = new google.maps.places.Autocomplete(end);
     end_complete.bindTo('bounds', map);
-    
-    google.maps.event.addListener(start_complete, 'place_changed', function() {
+<<<<<<< HEAD
 
+    google.maps.event.addListener(start_complete, 'place_changed', function() {
+=======
+>>>>>>> 60cdc86d7ea16536bc69008972e323ed4538cd7a
+
+    google.maps.event.addListener(start_complete, 'place_changed', function() {
         var place = start_complete.getPlace();
         if (!place.geometry) {
           return;
@@ -47,15 +51,14 @@ function initialize() {
 
         if (place.geometry.viewport) {
           map.fitBounds(place.geometry.viewport);
-        } 
+        }
         else {
           map.setCenter(place.geometry.location);
-          map.setZoom(17);  // Why 17? Because it looks good.
+          map.setZoom(17);
        }
     });
 
     google.maps.event.addListener(end_complete, 'place_changed', function() {
-
         var place = end_complete.getPlace();
         if (!place.geometry) {
           return;
@@ -63,10 +66,10 @@ function initialize() {
 
         if (place.geometry.viewport) {
           map.fitBounds(place.geometry.viewport);
-        } 
+        }
         else {
           map.setCenter(place.geometry.location);
-          map.setZoom(17);  // Why 17? Because it looks good.
+          map.setZoom(17);
        }
     });
 }
